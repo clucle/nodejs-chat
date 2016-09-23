4b6b9d62f083b1d18c360d1f395df029e0ae313b
dude <wjdenwls123@gmail.com>
2016-09-23 23:35:53 +0900

    add whisper

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
-	nicknames = [];
+	users = {};
 
 server.listen(3000);
 
@@ -12,28 +12,47 @@ app.get('/', function(req, res){
 
 io.sockets.on('connection', function(socket){
 	socket.on('new user', function(data, callback){
-		if (nicknames.indexOf(data) != -1){
+		if (data in users){
 			callback(false);
 		} else{
 			callback(true);
 			socket.nickname = data;
-			nicknames.push(socket.nickname);
+			users[socket.nickname] = socket;
 			updateNicknaes();
 			
 		}
 	});
 
 	function updateNicknaes(){
-		io.sockets.emit('usernames', nicknames);
+		io.sockets.emit('usernames', Object.keys(users));
 	}
 
-	socket.on('send message', function(data){
-		io.sockets.emit('new message', {msg: data, nick: socket.nickname});
+	socket.on('send message', function(data, callback){
+		var msg = data.trim();
+		if(msg.substr(0,3) === '/w '){
+			msg = msg.substr(3);
+			var ind = msg.indexOf(' '); // 공백 있을때 까지가 이름
+			if(ind !== -1){
+				var name = msg.substring(0, ind);
+				var msg = msg.substring(ind + 1);
+				if(name in users){
+					users[name].emit('whisper', {msg: msg, nick: socket.nickname});
+					console.log('Whisper!');
+				} else{
+					callback('Error! Enter a valid user.');
+				}
+			} else{
+				callback('Error Please enter a message for your whisper');
+			}
+		} else{
+			io.sockets.emit('new message', {msg: msg, nick: socket.nickname});
+		}
+		
 	});
 
 	socket.on('disconnect', function(data){
 			if(!socket.nickname) return;
-			nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+			delete users[socket.nickname];
 			updateNicknaes();
 	});
 });
